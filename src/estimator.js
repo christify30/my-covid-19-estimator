66a712a4a4d7645b6d6d54ccfd9804b54440aa08
Methodmain <christify30@gmail.com>
2020-04-13 12:35:24 +0100

    problem one solved

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
+const output = {
+  data: {},
+  impact: {},
+  severeImpact: {}
+};
+
+const computeCurrentlyInfected = (data) => {
+  const impactCurrentlyInfected = data.reportedCases * 10;
+  const severeImpactCurrentlyInfected = data.reportedCases * 50;
+  output.severeImpact.currentlyInfected = severeImpactCurrentlyInfected;
+  output.severeImpact.infectionsByRequestedTime = severeImpactCurrentlyInfected * 1024;
+  output.impact.currentlyInfected = impactCurrentlyInfected;
+  output.impact.infectionsByRequestedTime = impactCurrentlyInfected * 1024;
+};
+
+const covid19ImpactEstimator = (data) => {
+  output.data = data;
+  computeCurrentlyInfected(data);
+  return output;
+};
 
 export default covid19ImpactEstimator;
