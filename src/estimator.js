8d5a17548741a39b78d9a6ae981b992960cfb481
Methodmain <christify30@gmail.com>
2020-04-13 14:46:54 +0100

    corrections made

--- src/estimator.js
+++ src/estimator.js
@@ -4,13 +4,34 @@ const output = {
   severeImpact: {}
 };
 
+const calculateTIme = (type, value) => {
+  let data;
+  switch (type) {
+    case 'days':
+      data = 2 ** Number.parseInt((value / 3), 10);
+      break;
+    case 'weeks':
+      data = 2 ** Number.parseInt(((value * 7) / 3), 10);
+      break;
+    case 'months':
+      data = 2 ** Number.parseInt(((value * 28) / 3), 10);
+      break;
+    default:
+      data = 1024;
+      break;
+  }
+
+  return data;
+};
+
 const computeCurrentlyInfected = (data) => {
-  const impactCurrentlyInfected = data.reportedCases * 10;
-  const severeImpactCurrentlyInfected = data.reportedCases * 50;
+  const time = calculateTIme(data.periodType, data.timeToElapse);
+  const impactCurrentlyInfected = (data.reportedCases) * 10;
+  const severeImpactCurrentlyInfected = (data.reportedCases) * 50;
   output.severeImpact.currentlyInfected = severeImpactCurrentlyInfected;
-  output.severeImpact.infectionsByRequestedTime = severeImpactCurrentlyInfected * 1024;
+  output.severeImpact.infectionsByRequestedTime = (severeImpactCurrentlyInfected) * time;
   output.impact.currentlyInfected = impactCurrentlyInfected;
-  output.impact.infectionsByRequestedTime = impactCurrentlyInfected * 1024;
+  output.impact.infectionsByRequestedTime = (impactCurrentlyInfected) * time;
 };
 
 const covid19ImpactEstimator = (data) => {
