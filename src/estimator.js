2263c3a9e10b5a11bc1549a8dffff26a786a59b3
Methodmain <christify30@gmail.com>
2020-04-13 13:37:57 +0100

    done

--- src/estimator.js
+++ src/estimator.js
@@ -7,9 +7,7 @@ const output = {
 const computeCurrentlyInfected = (data) => {
   const impactCurrentlyInfected = data.reportedCases * 10;
   const severeImpactCurrentlyInfected = data.reportedCases * 50;
-  output.severeImpact.currentlyInfected = severeImpactCurrentlyInfected;
   output.severeImpact.infectionsByRequestedTime = severeImpactCurrentlyInfected * 1024;
-  output.impact.currentlyInfected = impactCurrentlyInfected;
   output.impact.infectionsByRequestedTime = impactCurrentlyInfected * 1024;
 };
 
