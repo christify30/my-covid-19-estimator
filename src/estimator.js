398b414a3de5b9132ad43a56140f66902a215d2d
Methodmain <christify30@gmail.com>
2020-04-13 13:27:30 +0100

    estimate removed

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,16 @@
 const output = {
   data: {},
-  estimate: {
-    impact: {},
-    severeImpact: {}
-  }
+  impact: {},
+  severeImpact: {}
 };
 
 const computeCurrentlyInfected = (data) => {
   const impactCurrentlyInfected = data.reportedCases * 10;
   const severeImpactCurrentlyInfected = data.reportedCases * 50;
-  output.estimate.severeImpact.currentlyInfected = severeImpactCurrentlyInfected;
-  output.estimate.severeImpact.infectionsByRequestedTime = severeImpactCurrentlyInfected * 1024;
-  output.estimate.impact.currentlyInfected = impactCurrentlyInfected;
-  output.estimate.impact.infectionsByRequestedTime = impactCurrentlyInfected * 1024;
+  output.severeImpact.currentlyInfected = severeImpactCurrentlyInfected;
+  output.severeImpact.infectionsByRequestedTime = severeImpactCurrentlyInfected * 1024;
+  output.impact.currentlyInfected = impactCurrentlyInfected;
+  output.impact.infectionsByRequestedTime = impactCurrentlyInfected * 1024;
 };
 
 const covid19ImpactEstimator = (data) => {
